23d82be1be1ac6fa69a8817f0f04eec4717c4760
7Liang <jun@wukecong.cn>
2022-05-02 14:04:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,18 @@ import Middleware from './middleware'
 import Bot from './bot'
 import Message from './message'
 
-export { Bot, Message, Middleware }
+import {
+    PrivateMessageData, GroupMessageData, GroupUploadNoticeData, GroupAdminNoticeData, GroupDecreaseNoticeData, GroupIncreaseNoticeData,
+    GroupBanNoticeData, FriendAddNoticeData, GroupRecallNoticeData, FriendRecallNoticeData, FriendPokeNoticeData, GroupPokeNoticeData,
+    LuckyKingNoticeData, GroupHonorNoticeData, GroupCardNoticeData, OfflineFileNoticeData, FriendRequestData, GroupRequestData,
+    ClientStatusNoticeData, EssenceNoticeData
+} from './base-type'
+
+export { 
+    Bot, Message, Middleware,
+    PrivateMessageData, GroupMessageData, GroupUploadNoticeData, GroupAdminNoticeData, GroupDecreaseNoticeData, GroupIncreaseNoticeData,
+    GroupBanNoticeData, FriendAddNoticeData, GroupRecallNoticeData, FriendRecallNoticeData, FriendPokeNoticeData, GroupPokeNoticeData,
+    LuckyKingNoticeData, GroupHonorNoticeData, GroupCardNoticeData, OfflineFileNoticeData, FriendRequestData, GroupRequestData,
+    ClientStatusNoticeData, EssenceNoticeData
+}
+
